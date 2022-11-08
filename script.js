f5c8b126569fcc0046858d9727af701eccb1fa99
chiagger <busca.chiara.cb@gmail.com>
2022-11-08 20:59:15 +0100

    refactored using async wait

--- script.js
+++ script.js
@@ -1,34 +1,19 @@
 const refreshBtn = document.querySelector("#refresh");
 const img = document.querySelector('img');
 
-const promise1 = new Promise((resolve, reject) => {
+getCats();
+  
+refreshBtn.addEventListener("click", getCats);
+
+
+  async function getCats() {
     try {
-    function newImg() {
-        fetch('https://api.giphy.com/v1/gifs/translate?api_key=14wu7N28JDFbmZtWnZbuSG5CFVVpSP75&s=cats', { mode: 'cors' })
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (response) {
-            img.src =response.data.images.original.url;
-        });
-    }
-    
-    refreshBtn.addEventListener("click", newImg);
-    
-    fetch('https://api.giphy.com/v1/gifs/translate?api_key=14wu7N28JDFbmZtWnZbuSG5CFVVpSP75&s=cats', { mode: 'cors' })
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (response) {
-            img.src =response.data.images.original.url;
-        });
+        const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=14wu7N28JDFbmZtWnZbuSG5CFVVpSP75&s=cats', {mode: 'cors'});
+        const catData = await response.json();
+        img.src = catData.data.images.original.url;
     } catch (error) { //i hope it workz?
         alert("couldn't find gif this time :(");
 
     }
-  });
-  
 
-
-
-
+  }
